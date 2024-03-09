beb20b9ab7f7f6d5889f921d823a076c36b45fd1
Fhanze <fhanze.id@gmail.com>
2024-03-09 20:34:36 +0700

    new index

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faFire, faHouse, faMoon, faSignOut, faStar } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
+
+        const togActive = () => {
+            
+            const body = document.querySelector("body");
+            body.classList.toggle("dark");
+            // const body = document.querySelector("body"),
+            //     modeToggle = body.querySelector(".input-mode");
+            // modeToggle.addEventListener("click", () => {
+            
+                // })
+            }
   return (
     <nav className='sidebar'>
         <div className='brands flex items-center'>
@@ -12,15 +25,41 @@ const Sidebar = () => {
         <span className='logo_name '>fhanzeMarket</span>
         </div>
 
-        <div className='side-menu'>
+        <div className='side-menu mt-[40px] h-[calc(100vh-90px)] flex flex-col justify-between'>
             <ul className='side__links'>
                 <li><a href="">
+                <FontAwesomeIcon icon={faHouse} className='side_ico' />
                     <span className=''>Home</span>
                     </a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
+                    <li><a href="">
+                <FontAwesomeIcon icon={faFire} className='side_ico' />
+                    <span className=''>Trending</span>
+                    </a></li>
+                    <li><a href="">
+                <FontAwesomeIcon icon={faStar} className='side_ico' />
+                    <span className=''>Popular</span>
+                    </a></li>
+                    <li><a href="">
+                <FontAwesomeIcon icon={faHouse} className='side_ico' />
+                    <span className=''>Home</span>
+                    </a></li>
+            </ul>
+
+            <ul className='user-section'>
+            <li><a href="">
+                <FontAwesomeIcon icon={faSignOut} className='side_ico' />
+                    <span className=''>Logout</span>
+                    </a></li>
+            
+                    <li className='toggle-mode'><a href="">
+                <FontAwesomeIcon icon={faMoon} className='side_ico' />
+                    <span className=''>Dark Mode</span>
+                    </a>
+                    
+                    <button className='input-mode ' onClick={togActive} >
+                        <span className='switch-mode'></span>
+                    </button>
+                    </li>
             </ul>
         </div>
 
